454134f4b137b1f6d1f411db38c30c1da8337685
Paul Varache <varache.paul@gmail.com>
2022-05-11 08:12:14 +0100

    First commit

---
+++ MauiBorderBackgroundIOS/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiBorderBackgroundIOS;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		Do().ConfigureAwait(false);
+	}
+
+	private async Task Do()
+    {
+		await Task.Delay(2000);
+		third.StrokeThickness = 0;
+    }
+}
+
